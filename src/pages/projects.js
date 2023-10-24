48c8a231dc18a0ff4fa4e83407cde87b47e7709f
Quinn Hipp <99678158+quinnhipp@users.noreply.github.com>
2023-10-24 10:45:17 -0400

    Added projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,6 +4,9 @@ import PreviewList from "../components/previewContainer";
 import BitmojiLaptop from "../assets/Quinn_Laptop_High.png";
 import FlashDrive from "../assets/Flash-Drive.png";
 import Multithreading from "../assets/Multithreading.jpeg";
+import Todo from "../assets/ss_todo.png"
+import BioLogo from "../assets/bioLogo.jpg"
+import COLogo from "../assets/Crystal_Orchard_logo.png"
 
 export default function () {
   const [projects, setProjects] = useState([
@@ -18,13 +21,37 @@ export default function () {
       id: 1,
     },
     {
+      image: COLogo,
+      title: "Crystal Orchard Website",
+      link: "https://the-crystal-orchard.vercel.app/",
+      description: "A website that utilizes Nextjs, Typescript, TailwindCSS, and SQLite.",
+      buttonText: "View Website",
+      id: 2,
+    },
+    {
+      image: Todo,
+      title: "To-Do List",
+      link: "https://github.com/quinnhipp/todo-list",
+      description: "To-Do App that utilizes Nextjs, Typescript, TailwindCSS, and SQLite.",
+      buttonText: "View on Github",
+      id: 3,
+    },
+    {
+      image: BioLogo,
+      title: "Biohazard Printing Website",
+      link: "https://www.biohazardprinting.com/",
+      description: "A website that utilizes Nextjs, Typescript, TailwindCSS, and SQLite.",
+      buttonText: "View Website",
+      id: 4,
+    },
+    {
       image: Multithreading,
       title: "Multithreading Applications",
       link: "https://github.com/quinnhipp/Multithreading-Applications",
       description:
         "Implementation of POSIX-Pthreads to multi-thread Quicksort for sorting huge lists.",
       buttonText: "View on Github",
-      id: 2,
+      id: 6,
     },
     {
       image: FlashDrive,
@@ -32,7 +59,7 @@ export default function () {
       link: "https://github.com/quinnhipp/FAT32",
       description: "Created a FAT32 structure from scratch in Linux",
       buttonText: "View on Github",
-      id: 3,
+      id: 7,
     },
   ]);
 
